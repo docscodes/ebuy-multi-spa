9ed841b9c9c322feced88d3f89cfeca8dfd3c3c3
docscodes <docscodes@gmail.com>
2024-10-03 17:28:21 -0400

    getSessionStorage to checkout

--- apps/checkout/src/app/app.tsx
+++ apps/checkout/src/app/app.tsx
@@ -3,14 +3,35 @@ import { Container, Header as Text } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import ShoppingBasket from './basket';
 import { PRODUCT_LIST_MOCKS } from '@ebuy-multi-spa/mocks';
+import { getSessionStorage } from '@ebuy-multi-spa/utils';
 
+
+const createCompleteBasket = (allItems: any, quantities: any) => {
+  return allItems
+    .filter((item: any) => quantities[item.id])
+    .map((item: any) => {
+      return {
+        ...item,
+        quantity: quantities[item.id],
+      };
+    });
+};
 
 export function App() {
+
+  const basketFromStorage: any = getSessionStorage('shoppingBasket');
+  console.log('Basket: ', basketFromStorage);
+
+  const completeBasket = createCompleteBasket(
+    PRODUCT_LIST_MOCKS,
+    basketFromStorage
+  );
+
   return (
     <Container style={{ marginTop: '5rem' }}>
       <Header />
       <Text size="huge">Checkout</Text>
-      <ShoppingBasket basketList={PRODUCT_LIST_MOCKS} />
+      <ShoppingBasket basketList={completeBasket} />
     </Container>
   );
 }
