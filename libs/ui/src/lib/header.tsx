ca524fd0a357b4c26e6222acf13e3645ecdac235
docscodes <docscodes@gmail.com>
2024-10-03 17:15:23 -0400

    getSessionStorage to header

--- libs/ui/src/lib/header.tsx
+++ libs/ui/src/lib/header.tsx
@@ -1,6 +1,27 @@
 import { Menu, Container, Icon, Label } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { useEventListener } from 'usehooks-ts';
+import { getSessionStorage } from '@ebuy-multi-spa/utils';
+
+const getTotalBasketCount = (basket: any): any => {
+  return Object.values(basket).reduce((a: any, b: any) => a + b, 0);
+};
 
 export function Header() {
+  const [miniBasketCount, setMiniBasketCount] = useState(null);
+
+  useEffect(() => {
+    const basket: any = getSessionStorage('shoppingBasket');
+    const totalCount: any = getTotalBasketCount(basket);
+    setMiniBasketCount(totalCount);
+  }, []);
+
+  useEventListener('session-storage', () => {
+    const basket: any = getSessionStorage('shoppingBasket');
+    const totalCount: any = getTotalBasketCount(basket);
+    setMiniBasketCount(totalCount);
+  });
+
   return (
     <Menu fixed="top" inverted>
       <Container>
@@ -11,7 +32,7 @@ export function Header() {
         <Menu.Item position="right">
           <Label>
             <Icon name="shopping cart" />
-            00
+            {miniBasketCount}
           </Label>
         </Menu.Item>
       </Container>
@@ -24,8 +45,7 @@ const MenuItems = () => {
     <>
       {NAV_ITEMS.map((navItem, index) => (
         <Menu.Item key={index}>
-          <a href={navItem.href ?? '#'}>{navItem.label}</
-          a>
+          <a href={navItem.href ?? '#'}>{navItem.label}</a>
         </Menu.Item>
       ))}
     </>
