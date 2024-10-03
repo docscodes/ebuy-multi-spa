6bef6ee9b7f74b19f4fda45566d12d2fabe127d3
docscodes <docscodes@gmail.com>
2024-10-03 16:22:32 -0400

    add checkout list

--- apps/checkout/src/app/app.tsx
+++ apps/checkout/src/app/app.tsx
@@ -1,13 +1,16 @@
 import { Header } from '@ebuy-multi-spa/ui';
 import { Container, Header as Text } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import ShoppingBasket from './basket';
+import { PRODUCT_LIST_MOCKS } from '@ebuy-multi-spa/mocks';
 
 
 export function App() {
   return (
     <Container style={{ marginTop: '5rem' }}>
       <Header />
-      <Text size="huge">Checkout App</Text>
+      <Text size="huge">Checkout</Text>
+      <ShoppingBasket basketList={PRODUCT_LIST_MOCKS} />
     </Container>
   );
 }
