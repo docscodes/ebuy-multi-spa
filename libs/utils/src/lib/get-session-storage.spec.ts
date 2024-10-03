fc845c43598b7c58c4a2a5f6d4cdf6c952b6e3b4
docscodes <docscodes@gmail.com>
2024-10-03 17:02:04 -0400

    setup nx workspace library

---
+++ libs/utils/src/lib/get-session-storage.spec.ts
@@ -0,0 +1,7 @@
+import { getSessionStorage } from './get-session-storage';
+
+describe('utils', () => {
+  it('should work', () => {
+    // expect(getSessionStorage()).toEqual('utils');
+  });
+});
